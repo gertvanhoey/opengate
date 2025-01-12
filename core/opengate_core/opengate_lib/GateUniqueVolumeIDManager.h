2bd22c4a3845e2219bbea2ff9daa0aeb7c3e0dc7
Gert Van Hoey <gert.vanhoey@xeos.care>
2025-01-12 12:42:31 +0000

    Add possibility to use unordered_map instead of map in GateUniqueVolumeIDManager

--- core/opengate_core/opengate_lib/GateUniqueVolumeIDManager.h
+++ core/opengate_core/opengate_lib/GateUniqueVolumeIDManager.h
@@ -11,6 +11,7 @@
 #include "G4VTouchable.hh"
 #include "GateUniqueVolumeID.h"
 #include <string>
+#include <unordered_map>
 #include <utility>
 
 /*
@@ -31,11 +32,40 @@ protected:
 
   static GateUniqueVolumeIDManager *fInstance;
 
+  struct IDArrayTypeHash {
+    std::size_t operator()(const GateUniqueVolumeID::IDArrayType &arr) const {
+      std::size_t hashValue = 0;
+      std::hash<int> intHasher;
+      for (const auto &elem : arr) {
+        if (elem == -1)
+          break;
+        hashValue ^=
+            intHasher(elem) + 0x9e3779b9 + (hashValue << 6) + (hashValue >> 2);
+      }
+      return hashValue;
+    }
+  };
+
+  template <typename T1, typename T2> struct PairHash {
+    std::size_t operator()(const std::pair<T1, T2> &p) const {
+      std::size_t h1 = std::hash<T1>()(p.first);
+      std::size_t h2 = IDArrayTypeHash()(p.second);
+      return h1 ^ (h2 + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
+    }
+  };
+
   // Index of name + ID array to VolumeID
   // This map is created on the fly in GetVolumeID
   std::map<std::pair<std::string, GateUniqueVolumeID::IDArrayType>,
            GateUniqueVolumeID::Pointer>
       fToVolumeID;
+
+  /*
+    std::unordered_map<std::pair<std::string, GateUniqueVolumeID::IDArrayType>,
+                       GateUniqueVolumeID::Pointer,
+                       PairHash<std::string, GateUniqueVolumeID::IDArrayType>>
+        fToVolumeID;
+  */
 };
 
 #endif // GateUniqueVolumeIDManager_h
